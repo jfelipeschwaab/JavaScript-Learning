3a21bfd7541a1ee8a8ba70fbd85b50435dab453f
João Felipe <joaofsp.games@gmail.com>
2024-08-14 16:45:06 -0300

    html and Javascript class

---
+++ Aulas/Javascript + Html/Aula1.js
@@ -0,0 +1,16 @@
+//1. The script tag
+/*The <script> element allows you to add JavaScript code inside an HTML file. Below, the <script> element
+embeds valid JavaScript code:
+
+<script>
+  function Hello() {
+    alert ('Hello World');
+  }
+</script>
+*/
+
+
+//2.The src attribute
+//Para mirar o arquivo js no html, basta usar o src no script
+//<script src="meuArquivo.js"></script>
+//Dentro da tag Head no html
