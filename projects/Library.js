32c6b6647bbe63f29a2bcaab97e7bbdf508b4a82
João Felipe <joaofsp.games@gmail.com>
2024-09-03 14:38:56 -0300

    Adicionando recomendações codecademy no projeto de livraria

--- projects/Library.js
+++ projects/Library.js
@@ -67,3 +67,19 @@ class Movie extends Media {
         return this._runTime;
     }
 }
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAvarageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAvarageRating());
